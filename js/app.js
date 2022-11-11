be07c05942ad3f8b87585bcad032322fb7c0ed1d
LindseyLouv <lindsey.louv@gmail.com>
2022-11-11 19:30:57 +0100

    CSS ok

--- js/app.js
+++ js/app.js
@@ -1,47 +1,41 @@
-
 const app = {
     init: function() {
-        console.log("app.js init");
-        console.log(app.deck52Cards);
+        app.generateRandomDeck();
+
     },
 
-    // création d'un deck de 52 cartes
-    deck52Cards: {
-        'spade': {
-            '1': 14,
-            '2' : 2,
-            '3' : 3,
-            '4' : 4,
-            '5' : 5,
-            '6' : 6,
-            '7' : 7,
-            '8' : 8,
-            '9' : 9,
-            '10' : 10,
-            'J' : 11,
-            'Q' : 12,
-            'K' : 13,
-        },
-        'heart': {'1': 14,'2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9, '10' : 10, 'J' : 11, 'Q' : 12, 'K' : 13,},    
-        'club': {'1': 14,'2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9, '10' : 10, 'J' : 11, 'Q' : 12, 'K' : 13,},    
-        'diamond': {'1': 14,'2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9, '10' : 10, 'J' : 11, 'Q' : 12, 'K' : 13,},    
-    },
+    deckOfCards: [],
+    cardSymbols: ["club", "heart", "spade", "diamond"],
+    cardValue: [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"],
 
-    shuffleDeck: function(deckToShuffle) {
+    generateRandomDeck: function() {
+        // génération d'un deck de 52 cartes
+        i = 0;
+        for (y = 0; y <=3; y++) { 
+         //   app.deckOfCards[y] = app.cardSymbols[y];
+            for (let card of app.cardValue) {
+                
+                app.deckOfCards[i] = card;
+                i++;          
+            }
+        }
+        console.log(app.deckOfCards);
 
+        // mélange du deck
+        for (let i = app.deckOfCards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = app.deckOfCards[i];
+            app.deckOfCards[i] = app.deckOfCards[j];
+            app.deckOfCards[j] = temp;
+            }
+        
     }
+
+        
+
+   
+
 
 }
 
 document.addEventListener("DOMContentLoaded", app.init);
-
-// créer un tableau avec les 52 cartes
-// mélanger ce tableau
-// distribuer les 52 cartes aux deux joueurs
-// chaque joueur pose la premiere carte de son paquet sur la table
-// on compare les cartes
-// celui qui a la plus grande récupère toutes les cartes
-// s'il y a égalité (bataille), on repose une carte face cachée avant de reposer une carte à découvert
-// on effectue tout ca tant que le nb de carte des joueurs est >0
-
-
